5d801633d0fe9fe94e58f2e8b9901c4dd84c0627
JurekKiller <pawel5059@gmail.com>
2018-10-18 17:48:19 +0200

    Thresholding

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -10,7 +10,7 @@ public class Test {
         Tesseract tesseract = new Tesseract();
         try {
             tesseract.setDatapath("tessdata");
-            String text = tesseract.doOCR(new File("C:/Users/Pawel/Desktop/Projekt Inż/SerialDetector/sza_bin.PNG"));
+            String text = tesseract.doOCR(new File("C:/Users/Pawel/Desktop/Projekt Inż/SerialDetector/Adaptivemean_thresh_binary.jpg"));
             System.out.print(text);
         } catch (TesseractException e) {
             e.printStackTrace();
--- src/sample/Thresholding.java
+++ src/sample/Thresholding.java
@@ -12,13 +12,19 @@ public class Thresholding {
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+      //  int radius = (int)(doubleRadius + 0.5);
+      //  int kernelSize;
 
         //String file = "C:/Users/Pawel/Desktop/Projekt Inż/SerialDetector/a.png";
 
-        Mat src = imread("audi.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+        Mat src = imread("lic_plate.png", CV_LOAD_IMAGE_GRAYSCALE);
         Mat dst = new Mat();
 
-        Imgproc.adaptiveThreshold(src, dst, 125, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 11, 12);
+
+        //Imgproc.GaussianBlur(src,dst, new Size(kernelSize, kernelSize), radius);
+      // Imgproc.imwrite("Adaptivemean_thresh_binary.jpg", dst);
+         Imgproc.adaptiveThreshold(src, dst, 250, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 19, -15);
+
         Imgcodecs.imwrite("Adaptivemean_thresh_binary.jpg", dst);
 
     }
