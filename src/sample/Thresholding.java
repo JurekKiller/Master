8416e4fe988d3ad771c972e7f8c48f19256dfee4
JurekKiller <pawel5059@gmail.com>
2018-10-29 22:20:15 +0100

    Smoothing

--- src/sample/Thresholding.java
+++ src/sample/Thresholding.java
@@ -2,7 +2,6 @@ package sample;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -13,7 +12,8 @@ public class Thresholding {
     public static Mat ConvertThresholding(Mat srcImage) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
       //  Mat src = BottomHat.ConversionBlackHat();
-
+        int MAX_KERNEL_LENGTH = 31;
+        int DELAY_BLUR = 100;
       //  Mat srcImage = licenceClassifier.rectangleDetection();
         Mat dstImage = new Mat();
         Random rand = new Random();
@@ -24,7 +24,13 @@ public class Thresholding {
       //  Imgproc.GaussianBlur(srcImage,dstImage, new Size(kernelSize, kernelSize), radius);
         // Imgproc.imwrite("Adaptivemean_thresh_binary.jpg", dst);
         //   Imgproc.adaptiveThreshold(srcImage, dstImage, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 5, 10);
-        Imgproc.GaussianBlur(srcImage, dstImage,new Size(5,5), 0);
+
+        for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2) {
+            Imgproc.medianBlur(srcImage, dstImage, i);
+
+            Imgcodecs.imwrite("NonBinar/ConvertThresholdingMedian" + rand+".jpg", dstImage);
+
+        }
         Imgcodecs.imwrite("NonBinar/ConvertThresholding" + rand+".jpg", srcImage);
         return dstImage;
 
