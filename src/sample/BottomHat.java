388a5c06252f159e5931b659266aaf57bf65f875
JurekKiller <pawel5059@gmail.com>
2018-10-29 17:49:29 +0100

    streamy i testy

--- src/sample/BottomHat.java
+++ src/sample/BottomHat.java
@@ -12,7 +12,7 @@ public class BottomHat {
     public static Mat ConversionBlackHat(Mat srcImage, String name, String path) {
         int kernelSize =18;
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("name : " +name +"         path : " + path);
+        System.out.println("name: " +name +"         path : " + path);
         Mat matImgDst = new Mat();
         Mat matImgDstInv = new Mat();
         Mat element = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(2 * kernelSize + 1, 2 * kernelSize + 1),
